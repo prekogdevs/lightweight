0822d3968c297f9f5db77fa275765871894a02ad
prekogdevs <gergelyszebenyi@gmail.com>
2021-03-01 20:08:34 +0100

    FoodApiService separated

--- app/src/main/java/com/android/project/lightweight/api/retrofit/service/FoodApiService.kt
+++ app/src/main/java/com/android/project/lightweight/api/retrofit/service/FoodApiService.kt
@@ -1,24 +1,9 @@
-package com.android.project.lightweight.api
+package com.android.project.lightweight.api.retrofit.service
 
 import com.android.project.lightweight.api.retrofit.model.FoodResponse
-import com.android.project.lightweight.utilities.AppConstants.Companion.BASE_URL
-import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import kotlinx.coroutines.Deferred
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
-
-private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
-private val retrofit =
-    Retrofit
-        .Builder()
-        .addConverterFactory(MoshiConverterFactory.create(moshi))
-        .addCallAdapterFactory(CoroutineCallAdapterFactory())
-        .baseUrl(BASE_URL)
-        .build()
 
 interface FoodApiService {
     @GET("search")
@@ -26,12 +11,6 @@ interface FoodApiService {
         @Query("api_key") apiKey: String,
         @Query("query") query: String
     ): Deferred<FoodResponse>
-}
-
-object FoodApi {
-    val retrofitService: FoodApiService by lazy {
-        retrofit.create(FoodApiService::class.java)
-    }
 }
 
 
--- app/src/main/java/com/android/project/lightweight/data/SearchViewModel.kt
+++ app/src/main/java/com/android/project/lightweight/data/SearchViewModel.kt
@@ -4,8 +4,8 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.android.project.lightweight.api.FoodApi
 import com.android.project.lightweight.api.retrofit.model.Food
+import com.android.project.lightweight.api.retrofit.service.FoodApi
 import com.android.project.lightweight.utilities.AppConstants
 import kotlinx.coroutines.launch
 
@@ -15,14 +15,13 @@ class SearchViewModel : ViewModel() {
     val response: LiveData<List<Food>>
         get() = _response
 
-    fun getFoods(query : String) {
+    fun getFoods(query: String) {
         viewModelScope.launch {
             val getFoodsDeferred = FoodApi.retrofitService.getFoodsAsync(AppConstants.API_KEY, query)
             try {
                 val result = getFoodsDeferred.await()
                 _response.value = result.foods
-            }
-            catch (e : Exception) {
+            } catch (e: Exception) {
                 _response.value = listOf()
             }
         }
